32927bf97020e5a5f0b43d2bbf640020f6609bef
thienbao92 <thienbao.vudao@yahoo.com>
2015-03-12 17:45:33 +0200

    changes

---
+++ pages/example.php
@@ -0,0 +1,9 @@
+
+<?php include 'header.php';?>
+<?php include 'navigation.php';?>
+
+<!-- Content start from here -->
+<div id="example"></div>
+<!-- Contents ends here -->
+
+<?php include 'footer.php';?>  
